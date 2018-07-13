ae19d71ac1a6a3d3a56d603a840b7998804c4ecb
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-07-13 10:02:58 +0530

    Updated the middleware to log Request Body

--- Xperiments.Middleware/SimpleMiddleware.cs
+++ Xperiments.Middleware/SimpleMiddleware.cs
@@ -2,6 +2,8 @@
 {
     using LendFoundry.Foundation.Logging;
     using Microsoft.AspNetCore.Http;
+    using System.IO;
+    using System.Text;
     using System.Threading.Tasks;
 
     public class SimpleMiddleware
@@ -18,7 +20,35 @@
         public async Task InvokeAsync(HttpContext context)
         {
             _logger.Info("SimpleMiddleware.InvokeAsync()");
-            await _next(context);
+
+            var injectedRequestStream = new MemoryStream();
+
+            try
+            {
+                var requestLog = $"REQUEST HttpMethod: {context.Request.Method}, Path: {context.Request.Path}";
+
+                using (var bodyReader = new StreamReader(context.Request.Body))
+                {
+                    var bodyAsText = bodyReader.ReadToEnd();
+                    if (string.IsNullOrWhiteSpace(bodyAsText) == false)
+                    {
+                        requestLog += $", Body: {bodyAsText}";
+                    }
+
+                    var bytesToWrite = Encoding.UTF8.GetBytes(bodyAsText);
+                    injectedRequestStream.Write(bytesToWrite, 0, bytesToWrite.Length);
+                    injectedRequestStream.Seek(0, SeekOrigin.Begin);
+                    context.Request.Body = injectedRequestStream;
+                }
+
+                _logger.Debug(requestLog);
+                await _next(context);
+            }
+            finally
+            {
+                injectedRequestStream.Dispose();
+            }
+            
         }
 
     }
