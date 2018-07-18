6198ee713b80dad0ae1614e0aa99c9437987ee2a
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-07-18 18:00:30 +0530

    Updated middleware to log response also

--- Xperiments.Middleware/RequestLoggingMiddleware.cs
+++ Xperiments.Middleware/RequestLoggingMiddleware.cs
@@ -2,6 +2,7 @@
 {
     using LendFoundry.Foundation.Logging;
     using Microsoft.AspNetCore.Http;
+    using System.Diagnostics;
     using System.IO;
     using System.Text;
     using System.Threading.Tasks;
@@ -21,6 +22,34 @@
         {
             _logger.Info("SimpleMiddleware.InvokeAsync()");
 
+            var timer = Stopwatch.StartNew();
+
+            _logger.Debug(await LogRequest2(context));
+            timer.Stop();
+            _logger.Debug($"Request logging took time: {timer.ElapsedMilliseconds}");
+
+            var originalBodyStream = context.Response.Body;
+
+            using (var responseBody = new MemoryStream())
+            {
+                context.Response.Body = responseBody;
+
+                await _next(context);
+
+                timer = Stopwatch.StartNew();
+
+                _logger.Debug(await LogResponse(context));
+
+                await responseBody.CopyToAsync(originalBodyStream);
+
+                timer.Stop();
+                _logger.Debug($"Response logging took time: {timer.ElapsedMilliseconds}");
+            }
+
+        }
+
+        private async Task<string> LogRequest(HttpContext context)
+        {
             // Body Logging as described in http://www.palador.com/2017/05/24/logging-the-body-of-http-request-and-response-in-asp-net-core/
 
             var injectedRequestStream = new MemoryStream();
@@ -43,15 +72,53 @@
                     context.Request.Body = injectedRequestStream;
                 }
 
-                _logger.Debug(requestLog);
-                await _next(context);
+                return requestLog;
             }
             finally
             {
-                injectedRequestStream.Dispose();
+                //injectedRequestStream.Dispose();
             }
-            
         }
 
+        private async Task<string> LogRequest2(HttpContext context)
+        {
+            var requestLog = $"REQUEST ";
+
+            var injectedRequestStream = new MemoryStream();
+
+            await Task.Run(() =>
+            {
+                requestLog = $"HttpMethod: {context.Request.Method}, Path: {context.Request.Path}";
+                using (var bodyReader = new StreamReader(context.Request.Body))
+                {
+                    var bodyAsText = bodyReader.ReadToEnd();
+                    if (string.IsNullOrWhiteSpace(bodyAsText) == false)
+                    {
+                        requestLog += $", Body: {bodyAsText}";
+                    }
+                    else
+                    {
+                        requestLog += $", Body: <empty>";
+                    }
+
+                    // Copy data and push it back in the context
+                    var bytesToWrite = Encoding.UTF8.GetBytes(bodyAsText);
+                    injectedRequestStream.Write(bytesToWrite, 0, bytesToWrite.Length);
+                    injectedRequestStream.Seek(0, SeekOrigin.Begin);
+                    context.Request.Body = injectedRequestStream;
+                }
+            });
+            
+            return requestLog;
+        }
+
+        private async Task<string> LogResponse(HttpContext context)
+        {
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+            var text = await new StreamReader(context.Response.Body).ReadToEndAsync();
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+
+            return $"Response {text}";
+        }
     }
 }
