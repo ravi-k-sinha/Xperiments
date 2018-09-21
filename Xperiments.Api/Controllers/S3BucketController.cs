a10edc02a646b8832428fd8b9a5fc9ee9da1f79c
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-09-21 18:39:28 +0530

    Implemented functionality for adding an s3 bucket. Specified credentials in the home directory, created a note, added dependencies in Startup

--- Xperiments.Api/Controllers/S3BucketController.cs
+++ Xperiments.Api/Controllers/S3BucketController.cs
@@ -1,11 +1,26 @@
 namespace Xperiments.Api.Controllers
 {
+    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
+    using Service;
 
     [Produces("application/json")]
     [Route("api/S3Bucket")]
-    public class S3BucketController
+    public class S3BucketController : Controller
     {
+        private readonly IS3Service _service;
+
+        public S3BucketController(IS3Service service)
+        {
+            _service = service;
+        }
         
+        [HttpPost("{bucketName}")]
+        public async Task<IActionResult> CreateBucket([FromRoute] string bucketName)
+        {
+            var response = await _service.CreateBucketAsync(bucketName);
+
+            return Ok(response);
+        }
     }
 }
--- Xperiments.Service/S3Service.cs
+++ Xperiments.Service/S3Service.cs
@@ -1,13 +1,63 @@
 namespace Xperiments.Service
 {
+    using System;
+    using System.Net;
     using System.Threading.Tasks;
+    using Amazon.S3;
+    using Amazon.S3.Model;
+    using Amazon.S3.Util;
     using Models.S3;
 
-    class S3Service : IS3Service
+    public class S3Service : IS3Service
     {
-        public Task<S3Response> CreateBucketAsync(string bucketName)
+        private readonly IAmazonS3 _client;
+
+        public S3Service(IAmazonS3 client)
         {
-            throw new System.NotImplementedException();
+            _client = client;
+        }
+        
+        public async Task<S3Response> CreateBucketAsync(string bucketName)
+        {
+            try
+            {
+                var exists = await AmazonS3Util.DoesS3BucketExistAsync(_client, bucketName);
+
+                if (exists)
+                {
+                    return CreateS3Response(HttpStatusCode.BadRequest, $"[{bucketName}] folder already exists");
+                }
+                
+                var putBucketRequest = new PutBucketRequest
+                {
+                    BucketName = bucketName,
+                    UseClientRegion = true
+                };
+
+                var response = await _client.PutBucketAsync(putBucketRequest);
+
+                return CreateS3Response(response.HttpStatusCode, response.ResponseMetadata.RequestId);
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return CreateS3Response(e.StatusCode, e.Message);
+            }
+            catch (Exception e)
+            {
+                return CreateS3Response(HttpStatusCode.InternalServerError, e.Message);
+            }
+
+            return CreateS3Response(HttpStatusCode.InternalServerError, "Something went wrong");
+        }
+
+        private static S3Response CreateS3Response(HttpStatusCode status, string message)
+        {
+            return new S3Response
+            {
+                Status = status,
+                Message = message
+            };
         }
     }
 }
